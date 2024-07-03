455f2f0a83f56c9518b74fe7e4cb502f3c58f14b
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-03 13:39:17 +0900

    Initial Commit

---
+++ HelloCoffee/HelloCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCoffeeApp.swift
+//  HelloCoffee
+//
+//  Created by Brandon Jones on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
