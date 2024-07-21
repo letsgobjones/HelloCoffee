d798afeed69cfd95676571a92581f999221384b1
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-21 16:52:59 +0900

    Created an AddCoffeeView

--- HelloCoffee/Views/AddCoffieView.swift
+++ HelloCoffee/Views/AddCoffieView.swift
@@ -8,8 +8,27 @@
 import SwiftUI
 
 struct AddCoffieView: View {
+  @State private var name: String = ""
+  @State private var coffeeName: String = ""
+  @State private var price: String = ""
+  @State private var coffeeSize: CoffeeSize = .medium
+  
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      Form {
+        TextField("Name", text: $name).accessibilityIdentifier("name")
+        TextField("Coffee Name", text: $coffeeName).accessibilityIdentifier("coffeeName")
+        TextField("Price", text: $price).accessibilityIdentifier("price")
+        Picker("Select size", selection: $coffeeSize) {
+          ForEach(CoffeeSize.allCases, id: \.rawValue) { size in
+            Text(size.rawValue).tag(size)
+          }
+        }.pickerStyle(.segmented)
+        
+        Button("Place Order") {
+        }.accessibilityIdentifier("placeOrderButton")
+          .centerHorizontally()
+        
+      }
     }
 }
 
