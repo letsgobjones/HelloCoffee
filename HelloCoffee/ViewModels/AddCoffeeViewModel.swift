b026304cd1f0af99267ea4b7e3d3b231db0d8684
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-21 18:16:08 +0900

    Created an AddCoffeeViewModel

---
+++ HelloCoffee/ViewModels/AddCoffeeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  AddCoffeeViewModel.swift
+//  HelloCoffee
+//
+//  Created by Brandon Jones on 7/21/24.
+//
+
+import Foundation
+
+
+
+struct AddCoffeeErrors {
+  var name: String = ""
+  var coffeeName: String = ""
+  var price: String = ""
+  
+}
