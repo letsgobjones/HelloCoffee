fda277ae2fe6ea01f190543d6c35f86471516a9f
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-07-21 18:14:55 +0900

    Created an extension to tell if a view is visible or not.

--- HelloCoffee/Extension/View+Extension.swift
+++ HelloCoffee/Extension/View+Extension.swift
@@ -1,5 +1,5 @@
 //
-//  Views+Extension.swift
+//  View+Extension.swift
 //  HelloCoffee
 //
 //  Created by Brandon Jones on 7/21/24.
@@ -19,6 +19,26 @@ extension View {
       Spacer()
     }
   }
+  
+  
+  
+  @ViewBuilder
+  func visable(_ value: Bool) -> some View {
+    switch value {
+      case true:
+      self
+      case false:
+      EmptyView()
+      
+      
+      
+    }
+  }
+  
+  
+  
+  
 }
+
 
 
